b9f6718dfd6c4085649a94be1ddaf500994b2273
Idriss Mortadi <idrissmortadi@gmail.com>
2025-12-21 21:10:03 +0100

    nvim: remove lualine config

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -5,33 +5,8 @@ require("config.lazy")
 -- Author: lokesh-krishna
 -- MIT license, see LICENSE for more details.
 
-require("lualine").setup({
-  options = {
-    component_separators = "",
-    section_separators = { left = "", right = "" },
-  },
-  sections = {
-    lualine_a = { { "mode", separator = { left = "" }, right_padding = 2 } },
-    lualine_b = { "filename", "branch" },
-    lualine_c = {
-      "%=", --[[ add your center components here in place of this comment ]]
-    },
-    lualine_x = {},
-    lualine_y = { "filetype", "progress" },
-    lualine_z = {
-      { "location", separator = { right = "" }, left_padding = 2 },
-    },
-  },
-  inactive_sections = {
-    lualine_a = { "filename" },
-    lualine_b = {},
-    lualine_c = {},
-    lualine_x = {},
-    lualine_y = {},
-    lualine_z = { "location" },
-  },
-  tabline = {},
-  extensions = {},
-})
-
 vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#698DDA" })
+
+vim.keymap.set("n", "<leader>k", function()
+  require("hover_log").hover_append()
+end, { silent = true })
