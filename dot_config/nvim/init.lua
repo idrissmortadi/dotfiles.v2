0637d5fab7f96129fa6f05e75a91f608f4792309
Idriss Mortadi <idrissmortadi@gmail.com>
2026-01-04 22:12:07 +0100

    remove comments and useless keymap

--- dot_config/nvim/init.lua
+++ dot_config/nvim/init.lua
@@ -1,12 +1,4 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
 
--- Bubbles config for lualine
--- Author: lokesh-krishna
--- MIT license, see LICENSE for more details.
-
 vim.api.nvim_set_hl(0, "WinSeparator", { fg = "#698DDA" })
-
-vim.keymap.set("n", "<leader>k", function()
-  require("hover_log").hover_append()
-end, { silent = true })
