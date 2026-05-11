c0390254d16d409c9e700e555ae4017d1b995dd5
Idriss Mortadi <idrissmortadi@gmail.com>
2026-05-11 22:07:33 +0200

    add keymaps for snippets

--- dot_config/nvim/lua/config/keymaps.lua
+++ dot_config/nvim/lua/config/keymaps.lua
@@ -29,3 +29,22 @@ vim.keymap.set(
   { noremap = true, silent = true, desc = "Yank filepath (with line range in visual)" }
 )
 vim.keymap.set("i", "jj", "<ESC>", { silent = true })
+
+local ls = require("luasnip")
+vim.keymap.set({ "i" }, "<C-J>", function()
+  ls.expand()
+end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<C-L>", function()
+  ls.jump(1)
+end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<C-H>", function()
+  ls.jump(-1)
+end, { silent = true })
+
+vim.keymap.set({ "i", "s" }, "<C-E>", function()
+  if ls.choice_active() then
+    ls.change_choice(1)
+  end
+end, { silent = true })
